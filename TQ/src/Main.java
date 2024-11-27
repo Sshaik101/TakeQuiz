cea64c95f4cfba0129a61e36af1626fb1daceb62
unknown <anikathakur212@gmail.com>
2024-11-27 11:22:59 -0500

    first commit

--- TQ/src/Main.java
+++ TQ/src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+@FunctionalInterface
+interface myInterface {
+    int calculate(int x, int y);
+}
+
+class Main {
+    public static void main(String args[]) {
+        // Scanner scan = new Scanner(System.in);
+        myInterface s = ((int x, int y) -> {
+            int a = 0;
+            for (int i = x; i <= y; i++) {
+                a += i;  
+            }
+            return a;  
+        });
+        /* 
+        System.out.print("What's your first number?");
+        int firstNum = scan.nextInt();
+        System.out.println();        
+        System.out.print("What's your second number?");
+        int secondNum = scan.nextInt();
+        int ans = s.calculate(firstNum, secondNum);  
+        System.out.println(ans);  
+        */
     }
 }
